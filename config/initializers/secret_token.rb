fd84f6fb8e130dac66d1fc61cbcccfbca7b91790
Andrew McGregor <me@andrewmcgregor.me>
2013-07-24 15:29:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PairingRoulette::Application.config.secret_token = 'bccff596d1abda10f0f94d897d1d99be32d8f0904ad63a169e7eb2f7ca3aa4f4d0efd6d8502dd29b5d7f4727300e370880deed568eb81f9bfd51468f8fe2425a'
