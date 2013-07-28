c06bca7f8b3f0dd35a35d5ee8d0b884a9e3d0471
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-27 20:05:59 -0500

    modified topic tests

--- spec/models/covered_topic_spec.rb
+++ spec/models/covered_topic_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe CoveredTopic do
 
   context "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_present_of(:email) }
+    it { should validate}
   end
 
   context "accessible attributes" do
