16d8af96c0ba82a86f6e6224f1e6d6af8e587b79
Rick Thomas <rickthomas1980@gmail.com>
2013-07-28 19:55:26 -0500

    added routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 PairingRoulette::Application.routes.draw do
  
-  root to: "interviews#show"
+  root to: "sessions#new"
   resources :interviews
 
   match '/api/update_code', to: 'api#update_code'
   match '/api/run_code', to: 'api#run_code'
+  match '/login', to: 'employees#login'
 
 end
