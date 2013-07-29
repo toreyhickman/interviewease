a7904befcd4c2fd02664bc4c363615699d32500f
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-29 17:01:05 -0500

    added form to go directly to an interview and added flash div to layout to hold error messages

---
+++ app/controllers/checkins_controller.rb
@@ -0,0 +1,12 @@
+class CheckinsController < ApplicationController
+
+  def create
+    if Interview.find_by_identifier(params[:identifier])
+      redirect_to interview_path(params[:identifier])
+    else
+      flash[:error] = "Couldn't find a interview for that key!"
+      render "sessions/new"
+    end
+  end
+
+end
