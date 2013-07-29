9e480287cf87288c6045b2c3ffc52899f39c0ef4
Rick Thomas <rickthomas1980@gmail.com>
2013-07-29 09:29:11 -0500

    added candidates routes and interviews/:identifier route

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ PairingRoulette::Application.routes.draw do
   resources :employees
   resources :challenges
   resources :topics
+  resources :candidates
+  match '/interviews/:identifier', to: 'interviews#show'
   match '/api/update_code', to: 'api#update_code'
   match '/api/run_code', to: 'api#run_code'
   match '/login', to: 'employees#login'
   match '/signup', to: 'companies#signup'
-
 end
