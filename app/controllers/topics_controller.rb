6252197adbde0aa0884acc4f5aa66d4334a464e4
Patrick Villanueva <Patrick@Patricks-MacBook-Pro.local>
2013-07-30 11:10:24 -0500

    modified topics controller and new view

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,3 +1,13 @@
 class TopicsController < ApplicationController
 
+	def new
+	  @company = Company.find(current_user.company.id)
+	  @topic = Topic.new
+	end
+
+	def create
+	  @topic = Topic.new(params[:topic])
+	  current_user.company.topics << @topic
+	end
+
 end
