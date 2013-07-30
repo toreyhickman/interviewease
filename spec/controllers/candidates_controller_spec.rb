1f006df3e8d72bf1b6aa437841b3e6e2ccb8ec84
dbc-apprentice <me@andrewmcgregor.me>
2013-07-29 19:58:14 -0500

    Create controller spec files

---
+++ spec/controllers/candidates_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe CandidatesController, :type => :controller do
+
+end
---
+++ spec/controllers/challenges_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe ChallengesController, :type => :controller do
+
+end
