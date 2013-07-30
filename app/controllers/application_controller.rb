9eac42ca804bb5a84de07190d5020d034c8851b2
Rick Thomas <rickthomas1980@gmail.com>
2013-07-29 19:19:26 -0500

    deleted logout method from application controller (now lives in sessions controller as 'create'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   # Application helper methods
   helper_method :current_user, :login
 
-  def logout
-     session[:user_id] = nil
-     redirect_to root_url
-   end
 
   def current_user
     Employee.find_by_id(session[:user_id])
