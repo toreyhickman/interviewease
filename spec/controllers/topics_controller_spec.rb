ce1240c0523f7653193a0c44d8ee0050690d5efb
Andrew McGregor <me@andrewmcgregor.me>
2013-08-01 15:35:37 -0500

    Add pending TopicsController tests

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -1,5 +1,59 @@
 require 'spec_helper'
 
 describe TopicsController, :type => :controller do
+  describe 'when authenticated' do
+    describe 'GET #new' do
+      it 'assigns the current users company to @company' do
+        pending
+      end
 
+      it 'assigns a new topic to @topic' do
+        pending
+      end
+
+      it 'renders the #new view' do
+        pending
+      end
+    end
+
+    describe 'POST #create' do
+      describe 'with valid attributes' do
+        it 'saves the topic to the database' do
+          pending
+        end
+
+        it 'redirects to the company page' do
+          pending
+        end
+      end
+
+      describe 'with invalid attributes' do
+        it 'does not save the topic to the database' do
+          pending
+        end
+
+        it 're-renders the :new view' do
+          pending
+        end
+      end
+    end
+  end
+
+  describe 'when unauthenticated' do
+    describe 'GET #new' do
+      it 'redirects to sign in page' do
+        pending
+      end
+    end
+
+    describe 'POST #create' do
+      it 'doesnt save the topic to the database' do
+        pending
+      end
+      
+      it 'redirects to the sign in page' do
+        pending
+      end
+    end
+  end
 end
