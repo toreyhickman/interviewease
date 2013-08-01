ff9dee67d11cc3ca703fc43a3fc8e1b7f87f94b4
Andrew McGregor <me@andrewmcgregor.me>
2013-08-01 00:03:47 -0500

    Refactor + todo comments on InterviewsController

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,28 +1,25 @@
 class InterviewsController < ApplicationController
 
-  def find_interview
-    Interview.find_by_identifier(params[:id])
-  end
-
-  def create
+  def create # ?????????
   end
 
   def show
-    @interview = find_interview
+    @interview = Interview.find_by_identifier(params[:id])
   end
 
   def update
-    @interview = find_interview
+    @interview = Interview.find_by_identifier(params[:id])
     if @interview.update_attributes(params[:interview])
       redirect_to new_interview_feedback_path(@interview)
     else
       flash[:notice] = "Something went wrong with saving the interview as complete."
-      redirect_to current_user
+      redirect_to current_user # Somewhere better to redirect?
     end
   end
 
-  def new_interview_feedack
-    @interview = Interview.last
-  end
+  # This code shouldn't just select last record. Not sure where being used so will fix with testing
 
+  # def new_interview_feedack
+  #   @interview = Interview.last
+  # end
 end
