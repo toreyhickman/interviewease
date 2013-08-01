633cac5cb3a7e9915c066d1c02913d752c05b6cf
Andrew McGregor <me@andrewmcgregor.me>
2013-08-01 15:35:04 -0500

    Add pending tests for SessionsController

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
 
 describe SessionsController, :type => :controller do
+  describe 'GET #new' do
 
+  end
+
+  describe 'POST #create' do
+    it 'assigns the requested employee to @employee' do
+      pending
+    end
+
+    describe 'with valid attributes' do
+      it 'sets the session' do
+        pending
+      end
+
+      it 'redirects to employee profile' do
+        pending
+      end
+    end
+
+    describe 'with invalid attributes' do
+      it 'doesnt set the session' do
+        pending
+      end
+
+      it 'redirects to the home page' do
+        pending
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'should clear all sessions' do
+      pending
+    end
+
+    it 'should redirect to the home page' do
+      pending
+    end
+  end
 end
