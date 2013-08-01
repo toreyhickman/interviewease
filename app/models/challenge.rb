32430a0408175ee3ad45089b666535aef53dd881
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-01 09:59:38 -0500

    update Challenge model and model tests to not require test_code value

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,12 +1,11 @@
 class Challenge < ActiveRecord::Base
   # Accessible attributes (not company_id or author_id)
-  attr_accessible :title, :description, :code, :test_code, :public
+  attr_accessible :title, :description, :code, :public
 
   # Validations
   validates_presence_of :title
   validates_presence_of :description
   validates_presence_of :code
-  validates_presence_of :test_code
 
   # Associations
   belongs_to :company
