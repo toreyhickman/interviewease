c1bc45cb9f3f5660e9db36342bbca9d7ac80bfa3
Andrew McGregor <me@andrewmcgregor.me>
2013-08-01 00:01:34 -0500

    Todo comment in Checkins controller

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -4,8 +4,10 @@ class CheckinsController < ApplicationController
     if Interview.find_by_identifier(params[:identifier])
       redirect_to interview_path(params[:identifier])
     else
+
       flash[:error] = "No interviews match that code"
       render "sessions/new", layout: 'landing'
+      
     end
   end
 
